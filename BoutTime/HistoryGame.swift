634ef25989f734b16cfa95fbfe75ab90e483e55a
Marcus Klausen <30321578>
2017-03-05 14:23:31 +0100

    struggling with the EventCollectionUnarchiver since my plist is an array containing dictionaries

--- BoutTime/HistoryGame.swift
+++ BoutTime/HistoryGame.swift
@@ -17,5 +17,71 @@ protocol HistoricalEvent {
 
 protocol HistoricalGame {
     var eventCollection: [HistoricalEvent] { get set }
+    var roundsPlayed: Int { get set }
+    var numberOfRounds: Int { get }
+    var timer: Int { get set }
+    
+    init(eventCollection: [HistoricalEvent])
+    
+    func nextRound(currentRound: Int)
+    func endGame()
+    func checkAnswer()
+    
+}
+
+struct Event: HistoricalEvent {
+    let date: Date
+    let statement: String
+    var placement: Int
+}
+
+enum EventCollectionError: Error {
+    case invalidResource
+    case conversionFailure
+    case invalidSelection
+}
+
+class PlistConverter {
+    static func dictionary(fromFile name: String, ofType type: String) throws -> [String: AnyObject] {
+        guard let path = Bundle.main.path(forResource: name, ofType: type) else {
+            throw EventCollectionError.invalidResource
+        }
+        
+        guard let array = NSDictionary(contentsOfFile: path) as? [String: AnyObject] else {
+            throw EventCollectionError.conversionFailure
+        }
+        
+        return array
+        
+    }
+}
+
+
+class EventCollectionUnarchiver {
+    static func eventCollection(fromArray array: [String: AnyObject]) throws -> [HistoricalEvent] {
+        var collection: [HistoricalEvent] = []
+        
+        for (key, value) in collection {
+            if  let eventCollection = value as? [String: Any],
+                let date = evenCollection["]
+        }
+    }
+}
+
+
+
+class GameTopic: HistoricalGame {
+    var eventCollection: [HistoricalEvent] = []
+    var roundsPlayed: Int = 0
+    let numberOfRounds: Int = 6
+    var timer: Int = 60
+    
+    required init(eventCollection: [HistoricalEvent]) {
+        self.eventCollection = eventCollection
+    }
+    
+    func nextRound(currentRound: Int) {}
+    func endGame() {}
+    func checkAnswer() {}
     
 }

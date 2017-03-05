4c65fbb98083d8e4249059580321fca81737a620
Marcus Klausen <30321578>
2017-03-05 10:55:23 +0100

    Initial Commit

---
+++ BoutTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoutTime
+//
+//  Created by Marcus Jepsen Klausen on 05/03/2017.
+//  Copyright © 2017 Marcus Jepsen Klausen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

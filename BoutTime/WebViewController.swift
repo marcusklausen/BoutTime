0b97bb66445d3176e5bede9866795bc018dada8a
howroot <howroot@howroots-MacBook-Pro.local>
2017-04-11 20:54:19 +0200

    test

--- BoutTime/WebViewController.swift
+++ BoutTime/WebViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class WebViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad() 
 
         // Do any additional setup after loading the view.
     }
